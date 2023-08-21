495cfe2b8cad12bbf89f291c21d991ef060429e8
Tzu-Mao Li <tzumao@mit.edu>
2023-08-20 20:59:00 -0400

    HW1.1 input

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -1,7 +1,28 @@
 #include "hw1.h"
 
 Image3 hw_1_1(const std::vector<std::string> &params) {
+    // Homework 1.1: render a circle at the specified
+    // position, with the specified radius and color.
+
     Image3 img(640 /* width */, 480 /* height */);
+
+    Vector2 center = Vector2{img.width / 2 + Real(0.5), img.height / 2 + Real(0.5)};
+    Real radius = 10.0;
+    Vector3 color = Vector3{0.5, 0.5, 0.5};
+    for (int i = 0; i < (int)params.size(); i++) {
+        if (params[i] == "-center") {
+            Real x = std::stof(params[++i]);
+            Real y = std::stof(params[++i]);
+            center = Vector2{x, y};
+        } else if (params[i] == "-radius") {
+            radius = std::stof(params[++i]);
+        } else if (params[i] == "-color") {
+            Real r = std::stof(params[++i]);
+            Real g = std::stof(params[++i]);
+            Real b = std::stof(params[++i]);
+            color = Vector3{r, g, b};
+        }
+    }
 
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
