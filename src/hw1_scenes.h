fb589c02d3426165e8d223d93fb7d65ee84d1c6c
Tzu-Mao Li <tzumao@mit.edu>
2023-08-21 20:57:38 -0400

    add background to scene

--- src/hw1_scenes.h
+++ src/hw1_scenes.h
@@ -8,11 +8,13 @@ struct Circle {
 
 struct CircleScene {
     Vector2i resolution;
+    Vector3 background;
     std::vector<Circle> objects;
 };
 
 CircleScene hw1_2_scene_0 = {
     {640, 360}, // resolution
+    {0.5, 0.5, 0.5}, // background
     { // center, radius, color
         {{320, 180}, 160, {0.3, 0.5, 0.8}},
         {{150,  80},  80, {0.8, 0.3, 0.5}},
