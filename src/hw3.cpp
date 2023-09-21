c4ab6161f157d2514677fc3a36907bab15817efd
Tzu-Mao Li <tzumao@mit.edu>
2023-09-20 18:14:01 -0700

    indentation fix

--- src/hw3.cpp
+++ src/hw3.cpp
@@ -3,5 +3,5 @@
 #include "3rdparty/glfw/include/GLFW/glfw3.h"
 
 void hw_3_1(const std::vector<std::string> &params) {
-	// HW 3.1: Open a window using GLFW
+    // HW 3.1: Open a window using GLFW
 }
