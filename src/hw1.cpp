b590567762463516abab4ac62b950f6db1f1f10c
Tzu-Mao Li <tzumao@mit.edu>
2025-09-21 17:42:34 -0700

    Revert "HW1.2 update"
    This reverts commit 61e2d2fb5ae2861eeab9b7fc7e5923cd88e15f58.

--- src/hw1.cpp
+++ src/hw1.cpp
@@ -36,49 +36,15 @@ Image3 hw_1_1(const std::vector<std::string> &params) {
 }
 
 Image3 hw_1_2(const std::vector<std::string> &params) {
-    // Homework 1.2: render polylines
+    // Homework 1.2: render multiple circles
     if (params.size() == 0) {
         return Image3(0, 0);
     }
 
-    Image3 img(640 /* width */, 480 /* height */);
-    std::vector<Vector2> polyline;
-    // is_closed = true indicates that the last point and
-    // the first point of the polyline are connected
-    bool is_closed = false;
-    std::optional<Vector3> fill_color;
-    std::optional<Vector3> stroke_color;
-    Real stroke_width = 1;
-    for (int i = 0; i < (int)params.size(); i++) {
-        if (params[i] == "-points") {
-            while (params.size() > i+1 &&
-                    params[i+1].length() > 0 &&
-                    params[i+1][0] != '-') {
-                Real x = std::stof(params[++i]);
-                Real y = std::stof(params[++i]);
-                polyline.push_back(Vector2{x, y});
-            }
-        } else if (params[i] == "--closed") {
-            is_closed = true;
-        } else if (params[i] == "-fill_color") {
-            Real r = std::stof(params[++i]);
-            Real g = std::stof(params[++i]);
-            Real b = std::stof(params[++i]);
-            fill_color = Vector3{r, g, b};
-        } else if (params[i] == "-stroke_color") {
-            Real r = std::stof(params[++i]);
-            Real g = std::stof(params[++i]);
-            Real b = std::stof(params[++i]);
-            stroke_color = Vector3{r, g, b};
-        } else if (params[i] == "-stroke_width") {
-            stroke_width = std::stof(params[++i]);
-        }
-    }
+    int scene_id = std::stoi(params[0]);
+    const CircleScene &scene = hw1_2_scenes[scene_id];
 
-    if (fill_color && !is_closed) {
-        std::cout << "Error: can't have a non-closed shape with fill color." << std::endl;
-        return Image3(0, 0);
-    }
+    Image3 img(scene.resolution.x, scene.resolution.y);
 
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
